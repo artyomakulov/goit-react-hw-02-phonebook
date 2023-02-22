41f319187a7359aa522448a874c898e4b28938a7
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-02-23 00:56:19 +0200

    buttin disabled

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { nanoid } from 'nanoid'
 
 
  class Form extends React.Component {
@@ -24,10 +25,13 @@ import React from 'react'
         this.setState({name: '', number: ''})
     }
 
+    nameId = nanoid();
+    telId = nanoid();
+
     render () {
         return (
             <form onSubmit={this.handleSubmit}>
-        <label htmlFor=''>
+        <label htmlFor={this.nameId}>
             Name <input
             type="text"
             name="name"
@@ -36,9 +40,10 @@ import React from 'react'
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            id={this.nameId}
           />
         </label>
-        <label htmlFor=''>
+        <label htmlFor={this.telId}>
             Number <input
             type="tel"
             name='number'
@@ -47,9 +52,10 @@ import React from 'react'
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            id={this.telId}
           />  
         </label>
-        <button type="submit">Add contact</button>
+        <button type="submit" disabled={this.state.name === '' || this.state.number === ''}>Add contact</button>
       </form>
         )
     }
